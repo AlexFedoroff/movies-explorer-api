069d72c5e38dd76e842f708ec7ced3995f5b60e0
Alex Fedoroff <alexanderfedorov@ALEXANDERs-Mac-mini.local>
2023-02-21 21:22:51 +0300

    consoleJWT

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
+  console.log(origin);
   if (corsOrigins.includes(origin)) {
-    console.log(origin);
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
